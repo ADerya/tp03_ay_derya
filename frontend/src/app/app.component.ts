c09449f158a35224bb29d716841b14d8f5bbcd7d
Derya <derya.ay26@hotmail.com>
2024-03-08 21:48:41 +0100

    Add TP3

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from  '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormulaireComponent } from './components/formulaire/formulaire.component';
+import { RecapitulatifComponent } from './components/recapitulatif/recapitulatif.component';
+import { TetiereComponent } from './components/tetiere/tetiere.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TetiereComponent, FormulaireComponent, FooterComponent, RecapitulatifComponent, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'tp02_ay_derya';
+}
